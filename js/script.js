4b42f605579f34319bae1f0e3636e1fe91a5bbac
Edvard <user.edvard@gmail.com>
2017-11-16 21:16:05 +0200

    Modified responsiv + add screens portfolio n contact me + modified screen certificate

--- js/script.js
+++ js/script.js
@@ -2,16 +2,15 @@ $(document).ready(function(){
   $(".regular").slick({
         dots: true,
         infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false
       });
-});
 
-$(document).ready(function() {
- 
   $(window).resize(function(){
   	var ScreenHeight = screen.height;
-    var windowWidth = $(window).width();
     if( ScreenHeight > 760)$("#item").addClass("item-set");
     else $("#item").removeClass("item-set");
   });
